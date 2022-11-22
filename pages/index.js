ffab535e5a5a6a61308432e95cc74c23cc0f6f6d
Strawnico <nicolelicyanne0@gmail.com>
2022-11-21 22:29:54 -0300

    add tailwind and update home page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import Sesi from "../public/Sesi.png";
 import Link from "next/link";
-import Retangulo from "../public/Retangulo.svg";
 import Masculo from "../public/masculo.png";
 
 // NICOLE ESTA FAZENDO ESSA PÁGINA
@@ -16,23 +15,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <nav>
+      <main className="wpp">
+        <nav className="flex bg-transparent fixed">
           <Image
             width="112"
             height="35"
-            className="logo"
+            className=" mt-9 ml-11"
             src={Sesi}
             alt="Logo do Senai"
           />
-          <lu>
-            <li>
+          <lu className="list-none text-lg flex ml-28">
+            <li className="mt-10 ml-7 ">
               <Link href="/" locale="home">
                 INICIO
               </Link>
             </li>
 
-            <li>
+            <li className="mt-10 ml-7 ">
               <Link
                 href="https://sesisc.org.br/sobre-sesi"
                 locale="home"
@@ -42,7 +41,7 @@ export default function Home() {
               </Link>
             </li>
 
-            <li>
+            <li className="mt-10 ml-7 ">
               <Link
                 href="https://transparencia.sesisc.org.br/fale-conosco?entidade=sesi"
                 locale="home"
@@ -53,23 +52,47 @@ export default function Home() {
             </li>
           </lu>
         </nav>
-        <Image className="retangulo" src={Retangulo} alt="Parede retangular" />
-        <Image className="masculo" src={Masculo} alt="Homem masculo aqui" />
-        <div className="pl frase">
-          <span className="construa">
+        {/* <Image
+          className=" float-right"
+          width="860"
+          src={Retangulo}
+          alt="Parede retangular"
+        /> */}
+        <Image
+          className=" absolute left-1/2 mt-32 md:flex hidden max-w-full h-auto"
+          src={Masculo}
+          alt="Homem masculo aqui"
+        />
+        <div className=" pl-16 flex pt-64 mr-[700px]">
+          <span className=" text-6xl font-normal">
             CONSTRUA A SAÚDE
-            <span className="perfeita">PERFEITA</span>
+            <span className="perfeita text-6xl font-bold italic align">
+              PERFEITA
+            </span>
           </span>
         </div>
-        <p className="pl venha">
+        <p className="pl-16 font-normal text-stone-400 text-xl flex mr-[900px]">
           Venha conheçar uma das melhores academia da região de São José
         </p>
-        <Link href="/cadastro" locale="home" className="botoes b1">
-          CRIE UMA CONTA
-        </Link>
-        <Link href="/cadastro" locale="home" className="botoes b2">
-          FAÇA LOGIN
-        </Link>
+        <div className="display-flex mt-12">
+          <Link
+            href="/cadastro"
+            locale="home"
+            className="b1 bg-sky-600 text-white ml-16 py-3 px-5 rounded-3xl"
+          >
+            CRIE UMA CONTA
+          </Link>
+          <Link
+            href="/login"
+            locale="home"
+            className=" ml-5 border-solid border-2 border-sky-600 text-sky-600 py-3 px-9 rounded-3xl"
+          >
+            FAÇA LOGIN
+          </Link>
+        </div>
+        <p className="flex justify-center relative pt-96 mt-96">
+          Conheça nossas Modalidades
+        </p>
       </main>
 
       {/* <footer className={styles.footer}>
